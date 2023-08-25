930b37fb2a712cc7957154c436c3110e0beca624
FOGA1004 <meditactionafg@gmail.com>
2023-08-25 12:32:16 -0600

    Rebase Main

--- src/app/components/pages/pagina-admin-proyectos/pagina-admin-proyectos.component.ts
+++ src/app/components/pages/pagina-admin-proyectos/pagina-admin-proyectos.component.ts
@@ -176,11 +176,4 @@ export class PaginaAdminProyectosComponent implements OnInit {
       this.gridApi.refreshCells();
     });
   }
-
-  refrescar() {
-    this.castingClient.castingGet(true).subscribe((data) => {
-      this.gridApi.setRowData(data);
-      this.gridApi.refreshCells();
-    });
-  }
 }
