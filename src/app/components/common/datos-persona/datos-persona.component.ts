94103e0a98bf4ddcf59b2a5e70c29869b8497077
desarrollo <mediaob11@gmail.com>
2023-10-20 11:20:48 -0600

    conflictos resueltos

--- src/app/components/common/datos-persona/datos-persona.component.ts
+++ src/app/components/common/datos-persona/datos-persona.component.ts
@@ -919,10 +919,4 @@ export class DatosPersonaComponent implements OnInit {
   onClosed(dismissedAlert: any): void {
     this.dismissible = false;
   }
-  pageTitleContent = [
-    {
-      title: 'Mi Perfil',
-      backgroundImage: 'assets/img/page-title/page-title2-d.jpg',
-    },
-  ];
 }
