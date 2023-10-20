047a3d1302572ddb8e010f38e3e8d4c792dc1798
desarrollo <mediaob11@gmail.com>
2023-10-20 11:02:02 -0600

    rebase main

--- src/app/components/common/datos-persona/datos-persona.component.ts
+++ src/app/components/common/datos-persona/datos-persona.component.ts
@@ -919,4 +919,10 @@ export class DatosPersonaComponent implements OnInit {
   onClosed(dismissedAlert: any): void {
     this.dismissible = false;
   }
+  pageTitleContent = [
+    {
+      title: 'Mi Perfil',
+      backgroundImage: 'assets/img/page-title/page-title2-d.jpg',
+    },
+  ];
 }
