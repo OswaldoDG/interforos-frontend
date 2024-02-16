fcea40d369678057d5c018a366d4b9ce41a46d84
FOGA1004 <meditactionafg@gmail.com>
2024-02-16 14:31:06 -0600

    Correcion Servicio Excel

--- src/app/services/Files/download-excel.service.ts
+++ src/app/services/Files/download-excel.service.ts
@@ -1,31 +1,40 @@
-import { HttpClient, HttpErrorResponse, HttpEvent, HttpHeaders, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHeaders,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Injectable, Inject, InjectionToken, Optional } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-
+import { API_BASE_URL } from '../api/api-promodel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DownloadExcelService {
-  private apiBaseUrl = 'https://localhost:7001/api/Casting';
+  private apiBaseUrl: string;
   httpClient: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Optional() @Inject(API_BASE_URL) baseUrl?: string
+  ) {
+    this.apiBaseUrl = baseUrl?? '';
+  }
 
-  descargarArchivoExcel(castingId: string){
-    const url = `${this.apiBaseUrl}/${castingId}/excel`;
+  descargarArchivoExcel(castingId: string) {
+    const url = `${this.apiBaseUrl}/api/Casting/${castingId}/excel`;
     console.log(url);
-    return this.http.get(url,{responseType: 'blob'});
+    return this.http.get(url, { responseType: 'blob' });
   }
 
   descargarArchivoExcel2(castingId: string): Observable<HttpResponse<Blob>> {
-    const url = `${this.apiBaseUrl}/${castingId}/excel`;
+    const url = `${this.apiBaseUrl}/api/Casting/${castingId}/excel`;
     return this.http.get(url, {
       responseType: 'blob',
-      observe: 'response'
+      observe: 'response',
     });
   }
-
-
-
 }
