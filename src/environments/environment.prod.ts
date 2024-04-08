f1a1be4382e83c63ac9b6389648dba76cad460a5
Oswaldo DG <oswaldo.dgmx@gmail.com>
2024-04-08 11:59:08 -0600

    CambioURLAPI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   catalogosAbiertos: false,
   production: true,
-  apiRoot: 'https://promodel-api.eccordion.com',
+  apiRoot: 'https://interforos.neurofant.app',
   recaptcha: {
     siteKey: '6Ld7KSMoAAAAAI0d2clQr6hO7hfPHYuYs2-sWvyM',
   },
